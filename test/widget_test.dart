f00204fb347bb7815ed8ff0702cd4efcefcc7f5e
Capi03 <cinthiacarrasco321@gmail.com>
2026-06-24 14:28:59 -0400

    probaremos

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pos_frontend_mobile/main.dart';
+import 'package:pos_frontend_mobile/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
